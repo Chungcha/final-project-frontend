6f0f57fa8f680efb6e80f960930b1cec767bbb84
chungcha <charlieschung@gmail.com>
2019-12-18 10:54:55 -0500

    deleted react sample elements

--- src/App.js
+++ src/App.js
@@ -54,22 +54,7 @@ class App extends React.Component{
       <div className="home-container" >
         {this.state.currentUser ? <Home submitFoodChoice={this.submitFoodChoice}/> : <Login updateCurrentUser={this.updateCurrentUser}/> }
           <Router>
-            <div className="">
-              <header className="App-header">
-              <button className="ui button">Follow</button>
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
+            
           </Router>
         </div>
     )
